1f2feaef2de53145eeb31d6bd725168f48cce287
elias695 <eliasslebgg@gmail.com>
2026-03-21 02:14:38 +0100

    Delete preload_clip.py

--- preload_clip.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ["TORCH_HOME"] = "/app/.cache/torch"
-import clip
-print("Téléchargement modèle CLIP ViT-B/32...")
-model, preprocess = clip.load("ViT-B/32", device="cpu", download_root="/app/.cache/clip")
-print("Modèle CLIP OK")
