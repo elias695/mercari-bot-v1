0e645f0bd5f0a0a100a7abcd294873e26d80a427
elias695 <eliasslebgg@gmail.com>
2026-03-21 17:23:48 +0100

    Update preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -1,4 +1,4 @@
-import torchvision.models as models
-print("Téléchargement MobileNetV2...")
-model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
-print("MobileNetV2 téléchargé et mis en cache OK")
+import torch
+print("Téléchargement DINOv2 ViT-L/14...")
+model = torch.hub.load("facebookresearch/dinov2", "dinov2_vitl14")
+print("DINOv2 téléchargé et mis en cache OK ✅")
