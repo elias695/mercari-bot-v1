2ecc26f956cbc2637df3a6dd1cd520b34b35375a
elias695 <eliasslebgg@gmail.com>
2026-03-21 17:53:51 +0100

    Update preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -1,4 +1,4 @@
 import torch
-print("Téléchargement DINOv2 ViT-L/14...")
-model = torch.hub.load("facebookresearch/dinov2", "dinov2_vitl14")
+print("Téléchargement DINOv2 ViT-S/14...")
+model = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
 print("DINOv2 téléchargé et mis en cache OK ✅")
